4ccefcb430f7f7530bfde3a2e870d5dfba2c9076
Ali Shalabi <alishalabi@Alis-MBP-2.attlocal.net>
2019-08-25 21:09:03 -0700

    User can see one playlist

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.playlistr
@@ -17,6 +18,31 @@ app = Flask(__name__)
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists.find())
+
+
+@app.route('/playlists/new')
+def playlists_new():
+    """CRUD - Helper: Render New Playlist Form"""
+    return render_template('playlists_new.html')
+
+
+@app.route('/playlists', methods=["POST"])
+def playlists_submit():
+    """CRUD: Create New Playlist"""
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlist_id = playlists.insert_one(playlist).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """CRUD - Helper: Render One Playlist"""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 
 if __name__ == '__main__':
