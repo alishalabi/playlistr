a6212d7ddf2d8561bffc7eef05cc6c5195b6f180
Ali Shalabi <alishalabi@Alis-MBP-2.attlocal.net>
2019-08-25 21:38:19 -0700

    Working to debug partial

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from werkzeug.exceptions import NotFound
 
 client = MongoClient()
 db = client.playlistr
@@ -23,7 +24,7 @@ def playlists_index():
 @app.route('/playlists/new')
 def playlists_new():
     """CRUD - Helper: Render New Playlist Form"""
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
 
 @app.route('/playlists', methods=["POST"])
@@ -43,6 +44,30 @@ def playlists_show(playlist_id):
     """CRUD - Helper: Render One Playlist"""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
+
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """CRUD - Helper: Render Edit Form """
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """CRUD: Update Playlist"""
+    if request.form.get('_method') == 'PUT':
+        updated_playlist = {
+            'title': request.form.get('title'),
+            'description': request.form.get('description'),
+            'videos': request.form.get('videos').split()
+        }
+        playlists.update_one(
+            {'_id': ObjectId(playlist_id)},
+            {'$set': updated_playlist})
+        return redirect(url_for('playlists_show', playlist_id=playlist_id))
+    else:
+        raise NotFound()
 
 
 if __name__ == '__main__':
